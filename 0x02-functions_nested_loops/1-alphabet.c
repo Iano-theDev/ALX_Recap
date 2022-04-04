f136558e71fca2481893a1baae84e94f6003436f
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-04-04 11:49:54 +0300

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void print_alphabet(void)
+{
+    int i = 97;
+
+    while (i<=122)
+    {
+        _putchar(i);
+        i++;
+    }
+    _putchar('\n');
+    return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 #include "_putchar.c"
+#include "1-main.c"
 int _putchar(char c);
+void print_alphabet(void);
+
 
 #endif // MAIN_H_INCLUDED

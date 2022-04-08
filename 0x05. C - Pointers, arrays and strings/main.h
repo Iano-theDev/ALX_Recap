8bc9864727b82e7f89da7b331bfc4bebc0e9aa97
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-04-08 22:02:40 +0300

    every other char of a string

--- 0x05. C - Pointers, arrays and strings/main.h
+++ 0x05. C - Pointers, arrays and strings/main.h
@@ -7,6 +7,9 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
+void _print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 
 

5c89e9d77a945d125f86ebdd06e9b0595fe24414
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-05-02 11:41:00 +0300

    >>

--- 0x12.C-Singly_linked_lists/list_1.c
+++ 0x12.C-Singly_linked_lists/list_1.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct node *nodePtr;
 /** 
@@ -9,10 +10,33 @@ typedef struct node *nodePtr;
 
 struct node
 {
-    int value;
+    int data;
     nodePtr next;
-}
+};
+
 
 typedef struct node node;
 
-int main ()
+int main (int argc, const char * argv[])
+{
+    nodePtr first = NULL;
+    first = malloc(sizeof (node));
+    first ->next = NULL;
+
+    //node firstNode;
+    //firstNode = *first;
+
+    first -> data = 61;
+
+   // first -> next = malloc (sizeof(node));
+   // first -> next = NULL;
+   // first -> next -> data -> 62;
+
+   nodePtr temp;
+   temp = malloc (sizeof(node));
+   temp -> next = first;
+   first = temp;
+
+    printf ("Hello World!\n");
+    return (0);
+}

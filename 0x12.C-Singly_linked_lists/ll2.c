25f15dde9be27a47e6e0e1ab9135bac39d02c51f
IAN KAMANDE <60983828+Iano-theDev@users.noreply.github.com>
2022-05-06 16:14:45 +0300

    New linked list

---
+++ 0x12.C-Singly_linked_lists/ll2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * this program is supposed to create a linked list of 5 items and
+ * then print them out.
+ */
+
+struct Node{
+    int data;
+    struct Node* next;
+};
+
+//function to print the linked list
+void printList(struct Node* n)
+{
+    while (n != NULL)
+    {
+        printf("%d, ", n->data);
+        n = n->next;
+    }
+}
+
+//creating the linked list with 5 nodes
+int main ()
+{
+    struct Node* head = NULL;
+    struct Node* second = NULL;
+    struct Node* third = NULL;
+    struct Node* fourth = NULL;
+    struct Node* fifth = NULL;
+
+    //Allocate the 5 Nodes in the heap
+    head = (struct Node*)malloc(sizeof(struct Node));
+    second = (struct Node*)malloc(sizeof(struct Node));
+    third = (struct Node*)malloc(sizeof(struct Node));
+    fourth = (struct Node*)malloc(sizeof(struct Node));
+    fifth = (struct Node*)malloc(sizeof(struct Node));
+
+    //five blocks have been allocated dynamically.
+
+    head -> data = 6; //assign data to the first Node
+    head -> next = second; //link the first Node to the second node
+
+    second -> data = 4;
+    second -> next = third; //link the second Node to the third
+
+    third -> data = 3;
+    third -> next = fourth; //link the third Node to the fourth
+
+    fourth -> data = 2;
+    fourth -> next = fifth;
+
+    fifth -> data = 1;
+    fifth -> next = NULL;
+
+    //now to print the list of 
+    printList(head);
+    return (0);
+}
